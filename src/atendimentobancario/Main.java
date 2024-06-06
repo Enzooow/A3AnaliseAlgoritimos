e7dc57f565f4c7e4b3edfd3a26e1b89b2565493e
João Lucas Yonea <assasinomortal321321@gmail.com>
2024-06-05 22:09:13 -0300

    Atualizações do projeto

--- src/atendimentobancario/Main.java
+++ src/atendimentobancario/Main.java
@@ -1,92 +1,134 @@
 package atendimentobancario;
 
 import TADS.Fila;
-import atendimentobancario.model.Cliente;
 import atendimentobancario.model.Guiche;
+import atendimentobancario.model.Cliente;
 import java.util.Random;
-import javax.xml.transform.OutputKeys;
 
 public class Main {
-    private static Guiche[] arrayGuiches = {new Guiche(1), new Guiche(2), new Guiche(3)};
+
     private static Fila fila = fila = new Fila();
-    private static int tempoExpediente = 21600;
-    private static int numeroClientes = 0;
+    private static Guiche[] guiches = {new Guiche(1), new Guiche(2), new Guiche(3)};
+    private static int numDepositos = 0;
+    private static int numSaques = 0;
+    private static int numPagamentos = 0;
+    private static int tempoRestanteGuiches = 0;
+    private static int tempoTotalCliente = 0;
+    private static int expediente = 21600;
     
     public static void main(String[] args) throws InterruptedException {
+
+        var temGuicheOcupado = false;
+        boolean filaDesocupada = true;
         
-        boolean temGuicheOcupado = false;
-        boolean filaVazia = true;
-        while(tempoExpediente >= 0 || !filaVazia || temGuicheOcupado) {
-            int qtdGuichesOcupados = 0;
-            filaVazia = fila.isEmpty();
-            System.out.println("Tempo Restante Expediente: " + gerarHorarioPorSegs(tempoExpediente--));
-            System.out.println("Quantidade de clientes na fila: " + fila.length);
-            Guiche guicheDisponivel = null;        
-            for(Guiche guiche : arrayGuiches) {                                
-                if(guiche.estaOcupado()) {
-                    System.out.println("Guiche " + guiche.getNumero() + " encerrará seu atendimento em " + guiche.tempoRestante() + " segundos.");
+        //Caso o expediente não tenha acabado ou tenha alguém sendo atendido ou tenha alguém na fila, todo o fluxo continuará rodando, pois não pode-se fechar a empresa com clientes dentro.
+        while (expediente >= 0  || temGuicheOcupado || !filaDesocupada) {
+            System.out.println("Tempo de expediente: " + gerarHorarioPorSegs(expediente--));
+            System.out.println("Clientes na fila: " + fila.length);
+            tempoRestanteGuiches = 0;
+            
+            Guiche guicheVazio = null;
+            
+            for (Guiche guiche : guiches) {     
+                //Se o guiche em questão está ocupado, não faz nada, apenas passa 1 segundo e verifica se algum cliente chegou.
+                if (guiche.ocupado()) {
                     guiche.removerTempoOcupacao(1);
-                    qtdGuichesOcupados++;
                     continue;
                 }
-                if(guicheDisponivel == null) {
-                    guicheDisponivel = guiche;
+                //Caso o guichê esteja disponível, haverá a possibilidade de um cliente ir até ele fazer uma operação.
+                guicheVazio = guiche;
+            }
+
+            //A cada segundo, ocorre um sorteio de 0 a 29 e se cair no zero, um cliente entra na fila.
+            if (expediente >= 0) {
+                if (sorteioCliente()) {
+                    Cliente cliente = new Cliente(expediente);
+                    fila.enqueue(cliente);
+                    System.out.println("Chegou um novo cliente.");
                 }
             }
             
-            temGuicheOcupado = qtdGuichesOcupados > 0;
-            
-            if(tempoExpediente >= 0) {
-                if(chegouCliente()) {
-                    Cliente cliente = new Cliente(tempoExpediente);
-                    System.out.println("O cliente chegou as " + gerarHorarioPorSegs(tempoExpediente));
-                    fila.enqueue(cliente);
-                }
-            } 
-//            else {
-//                System.out.println("Chegou cliente após tempo de expediente encerrar. Cliente expulso");
-//            }
-            
-            if(guicheDisponivel != null && !filaVazia) {
-                adicionarClienteGuiche(guicheDisponivel);
-            }                
+            //Se a tiver alguém na fila e um guichê vazio, o cliente vai até o guichê.
+            filaDesocupada = fila.isEmpty();
+            if (guicheVazio != null && !filaDesocupada) {
+                adicionarClienteGuiche(guicheVazio);
+            }
+
+            //Verifica se há algum guiche disponível a partir do tempo de atendimento restante em cada um.
+            for(var guiche : guiches){
+                tempoRestanteGuiches += guiche.tempoRestante();
+            }
+                
+            temGuicheOcupado = tempoRestanteGuiches > 0 ? true : false;
         }
-        
+
         System.out.println("Expediente encerrado.");
+
+        //Dados do dia obtidos:
+        var mediaTempoClientes = tempoTotalCliente / (numSaques + numDepositos + numPagamentos);
+        var clientesAtendidos = numSaques + numDepositos + numPagamentos;
+        System.out.println("O tempo médio de espera dos clientes foi de: " + mediaTempoClientes + " segundos");
+        System.out.println(clientesAtendidos + " clientes foram atendidos.");
+        System.out.println(numSaques + " clientes fizeram saque, " + numDepositos + ", clientes fizeram depositos e " + numPagamentos + " clientes fizeram pagamentos");
+        if (expediente < 0) {
+            System.out.println("Tempo extra de expediente: " + expediente * -1 + " segundos");
+        } else {
+            System.out.println("Não houve tempo extra de expediente");
+        }
     }
-    
+    public static boolean sorteioCliente() {
+        Random random = new Random(); return random.nextInt(30) == 0;
+    }
     public static void adicionarClienteGuiche(Guiche guiche) {
-        int numeroOperacao = gerarNumeroOperacao();
-        guiche.adicionarTempoOcupacao(numeroOperacao);
+        var numeroOperacao = gerarNumeroOperacao();
+        adicionarTempoOcupacao(guiche, numeroOperacao);
+
         Cliente cliente = fila.dequeue();
-        System.out.println("O cliente chegou as " + gerarHorarioPorSegs(cliente.getId()) + " foi chamado ao gichê " + guiche.getNumero());
+        tempoTotalCliente += cliente.getId() - expediente;
+        System.out.println("O cliente chegou as " + gerarHorarioPorSegs(cliente.getId()) + "e foi atendido no gichê " + guiche.getNumero());
     }
-    
-    public static boolean chegouCliente() {
-        Random random = new Random();
-        return random.nextInt(30) == 0;
-    }
-    
-    public static int gerarNumeroOperacao() {
-        Random random = new Random();
-        return random.nextInt(3);
-    }
+
+
     
     public static String gerarHorarioPorSegs(int segundos) {
         int horas = segundos / 3600;
         int minutos = (segundos % 3600) / 60;
         int segundosRestantes = segundos % 60;
-        
-        if(segundos < 0) {
-            horas = Math.abs(horas);
-            minutos = Math.abs(minutos);
-            segundosRestantes = Math.abs(segundosRestantes);
-        }
-        
-        String tempoRestante = String.format("%02d:%02d:%02d", horas, minutos, segundosRestantes);
 
-        return segundos < 0 ? "+" + tempoRestante : tempoRestante;
+        if (segundos < 0) {
+            segundosRestantes = segundosRestantes * -1;
+            minutos = minutos * -1;
+            horas = horas * -1;
+        }
+        String tempo = String.format("%02d:%02d:%02d", horas, minutos, segundosRestantes);
+
+        var string = "";
+        
+        if(segundos < 0)
+            string = "Hora extra: "+ tempo;
+        else
+            string = tempo;
+        
+        return string;
     }
     
-    
+    public static int gerarNumeroOperacao() {
+        Random random = new Random(); return random.nextInt(3);
+    }
+
+    public static void adicionarTempoOcupacao(Guiche guiche, int transacao) {
+        if (transacao == 0) {
+            System.out.println("O cliente realizará um saque. Ocupação: 60 segundos");
+            guiche.setTempoOcupacao(60);
+            numSaques++;
+        } else if (transacao == 1) {
+            System.out.println("O cliente realizará um depósito. Ocupação: 90 segundos");
+            guiche.setTempoOcupacao(90);
+            numDepositos++;
+        } else if (transacao == 2) {
+            System.out.println("O cliente realizará um pagamento. Ocupação: 120 segundos");
+            guiche.setTempoOcupacao(120);
+            numPagamentos++;
+        }
+    }
 }
--- src/atendimentobancario/model/Guiche.java
+++ src/atendimentobancario/model/Guiche.java
@@ -5,43 +5,18 @@ import atendimentobancario.Main;
 public class Guiche {
     private int numero;
     private int tempoOcupacao;
-    private int contadorSaques;
-    private int contadorPagamentos;
-    private int contadorDepositos;
 
     public Guiche(int numero) {
         this.numero = numero;
         tempoOcupacao = 0;
-        contadorSaques = 0;
-        contadorDepositos = 0;
     }
     
-    public boolean estaOcupado() {
+    public boolean ocupado() {
         return tempoOcupacao > 0;
     }
     
     public int tempoRestante() {
         return tempoOcupacao;
-    }
-    
-    public void adicionarTempoOcupacao(int transacao) {
-        switch(transacao) {
-            case 0:
-                System.out.println("O cliente realizará um saque. Ocupação: 60 segundos" );
-                tempoOcupacao += 60;
-                contadorSaques++;
-                break;
-            case 1:
-                System.out.println("O cliente realizará um depósito. Ocupação: 90 segundos" );
-                tempoOcupacao += 90;
-                contadorDepositos++;
-                break;
-            case 2:
-                System.out.println("O cliente realizará um pagamento. Ocupação: 120 segundos" );
-                tempoOcupacao += 120;
-                contadorPagamentos++;
-                break;
-        }
     }
     
     public void removerTempoOcupacao(int tempo) {
@@ -50,6 +25,10 @@ public class Guiche {
 
     public int getNumero() {
         return numero;
+    }
+    
+    public void setTempoOcupacao(int segs){
+        this.tempoOcupacao += segs;
     }
     
 }
